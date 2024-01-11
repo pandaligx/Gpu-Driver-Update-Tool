e2df22951709781b71f58aea26270aa4e60a6b78
李光雄 Guangxiong Li <156283479+pandaligx@users.noreply.github.com>
2024-01-11 13:46:11 +0800

    Add files via upload

---
+++ Project1_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef PROJECT1_PRIVATE_H
+#define PROJECT1_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.1.2"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	1
+#define VER_BUILD	2
+#define COMPANY_NAME	""
+#define FILE_VERSION	"1.0.1.2"
+#define FILE_DESCRIPTION	"显卡驱动升级助手"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Copyright (C) 2023 小南瓜"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"Gpu Driver Update Tool"
+#define PRODUCT_NAME	"显卡驱动升级助手"
+#define PRODUCT_VERSION	"1.0.1.2"
+
+#endif /*PROJECT1_PRIVATE_H*/
